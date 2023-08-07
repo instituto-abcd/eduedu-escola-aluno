5460ba09ce4fde7f62368bde6d164f72dbf0a9c0
bea <beatriztattooer@gmail.com>
2023-08-07 16:02:29 -0300

    :construction: wip[exams]: model20

---
+++ src/components/QuestionLoader/templates/Model20.tsx
@@ -0,0 +1,25 @@
+import { Flex, Group, SimpleGrid, Image } from "@mantine/core";
+import { Question } from "~/api/exam";
+import { OuvirIcon } from "~/assets/icons/Ouvir";
+import { DraggableCard } from "~/components/DraggableCard";
+import { EduButton } from "~/components/EduButton/EduButton";
+
+export function Model20({ question }: { question: Question }) {
+  return (
+    <>
+      <EduButton rightIcon={<OuvirIcon />}>Ouvir novamente</EduButton>
+
+      <Group position="apart" spacing={137}>
+        <SimpleGrid>
+          <Image src="https://place-hold.it/220" width={220} />
+          <Flex>
+            <DraggableCard
+              name="Caiu"
+              customHeigth="83px"
+            />
+          </Flex>
+        </SimpleGrid>
+      </Group>
+    </>
+  );
+}

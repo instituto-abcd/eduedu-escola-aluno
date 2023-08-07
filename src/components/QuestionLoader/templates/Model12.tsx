062eae333b653ff3674ab24829aaa129a8e11207
bea <beatriztattooer@gmail.com>
2023-08-07 15:26:46 -0300

    :sparkles: feat[exams]: model12

--- src/components/QuestionLoader/templates/Model12.tsx
+++ src/components/QuestionLoader/templates/Model12.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Group, Image, Stack } from "@mantine/core";
+import { Grid, Group, Image, Stack } from "@mantine/core";
 import { Question } from "~/api/exam";
 import { OuvirIcon } from "~/assets/icons/Ouvir";
 import { ArrowPlanet } from "~/components/ArrowPlanet/ArrowPlanet";
@@ -13,11 +13,18 @@ export function Model12({ question }: { question: Question }) {
       <Group position="apart" spacing={137}>
         <Stack align="center">
           <Image src="https://place-hold.it/362" width={269} height={175} mb={20} />
-          <SimpleGrid cols={3}>
-            <ArrowPlanet direction="prev" />
-            <DraggableCard />
-            <ArrowPlanet direction="next" />
-          </SimpleGrid>
+
+          <Grid>
+            <Grid.Col span="auto">
+              <ArrowPlanet direction="prev" />
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <DraggableCard customWidth="300px" />
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <ArrowPlanet direction="next" />
+            </Grid.Col>
+          </Grid>
         </Stack>
       </Group>
     </>

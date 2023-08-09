6c99c1685d1d828b8e48369f43f77619251fcc45
bea <beatriztattooer@gmail.com>
2023-08-09 09:00:00 -0300

    :pencil2: fix[dat]: model25

--- src/components/QuestionLoader/templates/Model25.tsx
+++ src/components/QuestionLoader/templates/Model25.tsx
@@ -34,19 +34,19 @@ export function Model25({ question }: { question: Question }) {
 
             <Grid.Col span={1}>
               <DraggableCard
-                image=""
+                image="https://place-hold.it/110"
                 name="O alho"
               />
             </Grid.Col>
             <Grid.Col span={1}>
               <DraggableCard
-                image=""
+                image="https://place-hold.it/110"
                 name="A rua"
               />
             </Grid.Col>
             <Grid.Col span={1}>
               <DraggableCard
-                image=""
+                image="https://place-hold.it/110"
                 name="O relógio"
               />
             </Grid.Col>

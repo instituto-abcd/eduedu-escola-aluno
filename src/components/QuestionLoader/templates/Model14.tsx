7db9e7c23884a501c021dccac086998b37a6e444
bea <beatriztattooer@gmail.com>
2023-08-17 14:58:16 -0300

    :sparkles: feat[planets]: model14

--- src/components/QuestionLoader/templates/Model14.tsx
+++ src/components/QuestionLoader/templates/Model14.tsx
@@ -1,10 +1,37 @@
 import { Grid, Group, Image } from "@mantine/core";
+import { produce } from "immer";
+import { useCallback, useState } from "react";
 import { Question } from "~/api/exam";
 import { OuvirIcon } from "~/assets/icons/Ouvir";
 import { DraggableCard } from "~/components/DraggableCard";
+import { CardItem } from "~/components/DraggableCard/DraggableCard";
 import { EduButton } from "~/components/EduButton/EduButton";
 
 export function Model14({ question }: { question: Question }) {
+  const [slots, setSlots] = useState<Slot[]>(question.options.map(() => null));
+
+  const [options] = useState<CardItem[]>(
+    question.options.map((option) => ({
+      id: option.position,
+      type: "ANSWER_CARD",
+      imageUrl: option.image_url ?? "",
+      description: option.description,
+      position: option.position,
+    }))
+  );
+
+  const handleDrop = useCallback(function (
+    item: CardItem | null,
+    index: number
+  ) {
+    setSlots((state) =>
+      produce(state, (draft) => {
+        draft[index] = item;
+      })
+    );
+  },
+    []);
+
   return (
     <>
       <EduButton rightIcon={<OuvirIcon />}>Ouvir novamente</EduButton>
@@ -14,34 +41,17 @@ export function Model14({ question }: { question: Question }) {
           <Grid.Col span={4}>
             <Image src="https://place-hold.it/110" height={256} />
           </Grid.Col>
-          <Grid.Col span={2} m="auto">
-            <DraggableCard
-              customWidth="80px"
-              customHeigth="80px"
-              name="1"
-            />
-          </Grid.Col>
-          <Grid.Col span={2} m="auto">
-            <DraggableCard
-              customWidth="80px"
-              customHeigth="80px"
-              name="2"
-            />
-          </Grid.Col>
-          <Grid.Col span={2} m="auto">
-            <DraggableCard
-              customWidth="80px"
-              customHeigth="80px"
-              name="3"
-            />
-          </Grid.Col>
-          <Grid.Col span={2} m="auto">
-            <DraggableCard
-              customWidth="80px"
-              customHeigth="80px"
-              name="4"
-            />
-          </Grid.Col>
+          {options
+            .sort((a, b) => a.position - b.position)
+            .map((item, inx) => (
+              <Grid.Col span={2} m="auto" key={item.id}>
+                <DraggableCard
+                  item={item}
+                  key={item.id}
+                  hidden={!!slots.find((slot) => slot?.id === item.id)}
+                />
+              </Grid.Col>
+            ))}
         </Grid>
       </Group>
     </>

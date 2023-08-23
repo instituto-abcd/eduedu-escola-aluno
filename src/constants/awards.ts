7538f83d44743f5c668052fef8ad5800f607da46
bea <beatriztattooer@gmail.com>
2023-08-23 09:23:17 -0300

    :fire: removing some awards

--- src/constants/awards.ts
+++ src/constants/awards.ts
@@ -1,8 +1,6 @@
 import bombastico from '~/assets/awards/bombastico.png'
 import brilhante from '~/assets/awards/brilhante.png'
 import competente from '~/assets/awards/competente.png'
-import confiante from '~/assets/awards/confiante.png'
-import corajoso from '~/assets/awards/corajoso.png'
 import escritor from '~/assets/awards/escritor.png'
 import excelente from '~/assets/awards/excelente.png'
 import fantastico from '~/assets/awards/fantastico.png'
@@ -14,7 +12,6 @@ import maestro from '~/assets/awards/maestro.png'
 import motivador from '~/assets/awards/motivador.png'
 import perfeito from '~/assets/awards/perfeito.png'
 import sensacional from '~/assets/awards/sensacional.png'
-import sensivel from '~/assets/awards/sensivel.png'
 import superAward from '~/assets/awards/super.png'
 
 export const AWARDS_IMAGES = [
@@ -29,14 +26,6 @@ export const AWARDS_IMAGES = [
     {
         name: 'competente',
         image: competente,
-    },
-    {
-        name: 'confiante',
-        image: confiante,
-    },
-    {
-        name: 'corajoso',
-        image: corajoso,
     },
     {
         name: 'escritor',
@@ -83,22 +72,7 @@ export const AWARDS_IMAGES = [
         image: sensacional,
     },
     {
-        name: 'sensivel',
-        image: sensivel,
-    },
-    {
         name: 'super',
         image: superAward,
-    },
-    // Awards without images:
-    //         sabio: '',
-    //         aptitude: '',
-    //         first_quiz: '',
-    //         fifth_quiz: '',
-    //         easy_peasy: '',
-    //         fighter: '',
-    //         astro: '',
-    //         socioemotional: '',
-    //         happy: '',
-    //         trabalhador: '',
+    }
 ]

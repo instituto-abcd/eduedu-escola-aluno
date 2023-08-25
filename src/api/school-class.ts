77475c05ebfcf82ddb28d0116ace26fae505288a
Guilherme Tabuada <ggtbh@outlook.com>
2023-08-25 17:56:25 -0300

    students by school class

--- src/api/school-class.ts
+++ src/api/school-class.ts
@@ -1,97 +1,107 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { API } from "./base";
 import { useCallback } from "react";
-import { MutationOptions } from "./api-types";
+import { MutationOptions, PaginationParams, QueryOptions } from "./api-types";
+import { Student } from "./student";
 
 const URL = {
-    GET_STUDENTS_BY_ID: (id: string) => `schoolClass/${id}/students`,
-    RESERVE_STUDENT: (id: string, studentId: string) => `schoolClass/${id}/students/${studentId}/reserved`
-}
+  GET_STUDENTS_BY_ID: (id: string) => `schoolClass/${id}/students`,
+  RESERVE_STUDENT: (id: string, studentId: string) =>
+    `schoolClass/${id}/students/${studentId}/reserved`,
+};
 
-type SchoolClass = {
-    id: string;
-    name: string;
+const KEY = {
+  GET_STUDENTS_BY_ID: "STUDENTS_BY_CLASS",
 };
 
 type ReserveStudent = {
-    id: string;
-    studentId: string;
+  id: string;
+  studentId: string;
 };
 
 export type SchoolGrade =
-    | "CHILDREN"
-    | "FIRST_GRADE"
-    | "SECOND_GRADE"
-    | "THIRD_GRADE";
+  | "CHILDREN"
+  | "FIRST_GRADE"
+  | "SECOND_GRADE"
+  | "THIRD_GRADE";
 export type SchoolPeriod = "MORNING" | "AFTERNOON" | "FULL";
 
 export class SchoolClassAPI extends API {
-    static async getStudentsById(id: string) {
-        const { data } = await this.api.get<SchoolClass>(URL.GET_STUDENTS_BY_ID(id))
-        return data
-    }
-    static async reserveStudent(id: string, studentId: string) {
-        const { data } = await this.api.patch(URL.RESERVE_STUDENT(id, studentId), { reserved: true })
-        return data
-    }
-    static async unreserveStudent(id: string, studentId: string) {
-        const { data } = await this.api.patch(URL.RESERVE_STUDENT(id, studentId), { reserved: false })
-        return data
-    }
+  static async getStudentsById(id: string, params?: PaginationParams) {
+    const { data } = await this.api.get<Student[]>(URL.GET_STUDENTS_BY_ID(id), {
+      params,
+    });
+    return data;
+  }
+
+  static async reserveStudent(id: string, studentId: string) {
+    const { data } = await this.api.patch(URL.RESERVE_STUDENT(id, studentId), {
+      reserved: true,
+    });
+    return data;
+  }
+
+  static async unreserveStudent(id: string, studentId: string) {
+    const { data } = await this.api.patch(URL.RESERVE_STUDENT(id, studentId), {
+      reserved: false,
+    });
+    return data;
+  }
 }
 
 export function useStudentsBySchoolclass(
-    options?: MutationOptions<
-        { id: string },
-        SchoolClass
-    >
+  schoolClassId: string,
+  options?: QueryOptions<Student[], [typeof KEY.GET_STUDENTS_BY_ID]>
 ) {
-    const handler = useCallback(function (data: {
-        id: string;
-    }) {
-        return SchoolClassAPI.getStudentsById(data.id);
+  const handler = useCallback(
+    function () {
+      return SchoolClassAPI.getStudentsById(schoolClassId, {
+        "page-number": options?.page,
+        "page-size": options?.pageSize,
+      });
     },
-        []);
+    [options?.page, options?.pageSize]
+  );
 
-    return useMutation(handler, options);
+  return useQuery([KEY.GET_STUDENTS_BY_ID], handler, options);
 }
 
 export function useReserveStudent(
-    options?: MutationOptions<
-        {
-            id: string,
-            studentId: string,
-        },
-        ReserveStudent
-    >
-) {
-    const handler = useCallback(function (data: {
-        id: string;
-        studentId: string;
-    }) {
-        return SchoolClassAPI.reserveStudent(data.id, data.studentId)
+  options?: MutationOptions<
+    {
+      id: string;
+      studentId: string;
     },
-        []);
+    ReserveStudent
+  >
+) {
+  const handler = useCallback(function (data: {
+    id: string;
+    studentId: string;
+  }) {
+    return SchoolClassAPI.reserveStudent(data.id, data.studentId);
+  },
+  []);
 
-    return useMutation(handler, options);
+  return useMutation(handler, options);
 }
 
 export function useUnreserveStudent(
-    options?: MutationOptions<
-        {
-            id: string,
-            studentId: string,
-        },
-        ReserveStudent
-    >
-) {
-    const handler = useCallback(function (data: {
-        id: string;
-        studentId: string;
-    }) {
-        return SchoolClassAPI.unreserveStudent(data.id, data.studentId)
+  options?: MutationOptions<
+    {
+      id: string;
+      studentId: string;
     },
-        []);
+    ReserveStudent
+  >
+) {
+  const handler = useCallback(function (data: {
+    id: string;
+    studentId: string;
+  }) {
+    return SchoolClassAPI.unreserveStudent(data.id, data.studentId);
+  },
+  []);
 
-    return useMutation(handler, options);
+  return useMutation(handler, options);
 }

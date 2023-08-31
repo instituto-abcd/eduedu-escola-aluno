e1c18769b1ee98345c90475edc5cce3cbd7aa7d7
Éder Franco <eder.f@lyncas.net>
2023-08-31 16:58:00 -0300

    Adicionando Lottie na página de Avaliação de Prova

--- src/pages/Exam/Exam-Evaluation.tsx
+++ src/pages/Exam/Exam-Evaluation.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useSubmitExamEvaluation } from "~/api/student";
 import bg from '~/assets/bgs/bg-exam-evaluation.jpg'
 import { PATH } from "~/constants/path";
+import Lottie from 'react-lottie';
+import loadingLottie from '~/assets/lotties/lottie_loading_anim.json'
 
 export function ExamEvaluationPage() {
 
@@ -12,15 +14,29 @@ export function ExamEvaluationPage() {
         onSuccess: () => navigate(PATH.DASHBOARD),
     });
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loadingLottie,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     return (
         <BackgroundImage src={bg} h="100vh">
-            <Center h="100vh">
+            <Center>
                 <Box style={{ color: '#fff', padding: '0px 0', fontSize: 20 }}>
                     <Container>
-                        <Flex direction="column" align="center" justify="center">
-                        <span>Você acabou de concluir a prova.</span>
-                        <span style={{paddingBottom: 15 }}>Aguarde enquanto o sistema calcula as suas tarefas.</span>
-                        {true && <Loader />}
+                        <Lottie
+                            options={defaultOptions}
+                            height={400}
+                            width={400}
+                        />
+                        <Flex direction="column" align="center" justify="center">  
+                            <span>Você acabou de concluir a prova.</span>
+                            <span style={{paddingBottom: 15 }}>Aguarde enquanto o sistema calcula as suas tarefas.</span>
+                            {true && <Loader />}
                         </Flex>
                     </Container>
                 </Box>

5001c9c1ef1ab9117e9d8295c1a694e861399519
bea <beatriztattooer@gmail.com>
2023-09-27 13:04:29 -0300

    :bug: fix/model24

--- src/components/QuestionLoader/templates/Model24.tsx
+++ src/components/QuestionLoader/templates/Model24.tsx
@@ -61,124 +61,122 @@ export function Model24({ question, answerCallback }: ModelProps) {
   return (
     <>
       <ScrollArea w={850} h={(lousaHeight * 80) / 100}>
-        <Center pb={20}>
-          <Stack>
-            <Group mx="auto">
-              {audioTitles
-                .filter((title) => title.file_url)
-                .map((title, inx) =>
-                  inx === 0 ? (
-                    <AudioButton
-                      key={title.position}
-                      src={title.file_url ?? ""}
-                      autoPlay
-                    />
-                  ) : (
-                    <AudioButton
-                      key={title.position}
-                      src={title.file_url ?? ""}
-                      buttonProps={{
-                        icon: (
-                          <IconRotateClockwise
-                            style={{ transform: "rotateX(180deg)" }}
-                            size={30}
-                          />
-                        ),
-                      }}
-                    />
-                  )
-                )}
-            </Group>
-
-            {imageTitles.map(
-              (title) =>
-                title.file_url && (
-                  <Image
-                    mx="auto"
-                    src={title.file_url}
-                    width="auto"
-                    height={190}
-                    alt={title.placeholder}
-                    key={title.file_url}
+        <Stack>
+          <Group mx="auto">
+            {audioTitles
+              .filter((title) => title.file_url)
+              .map((title, inx) =>
+                inx === 0 ? (
+                  <AudioButton
+                    key={title.position}
+                    src={title.file_url ?? ""}
+                    autoPlay
+                  />
+                ) : (
+                  <AudioButton
+                    key={title.position}
+                    src={title.file_url ?? ""}
+                    buttonProps={{
+                      icon: (
+                        <IconRotateClockwise
+                          style={{ transform: "rotateX(180deg)" }}
+                          size={30}
+                        />
+                      ),
+                    }}
                   />
                 )
-            )}
+              )}
+          </Group>
 
-            {isTypeComplete && (
-              <Stack my="auto" align="center" spacing={100}>
-                {textTitles.find((title) =>
-                  title.placeholder.includes("completar")
-                )?.description && (
-                    <Title
-                      dangerouslySetInnerHTML={{
-                        __html:
-                          textTitles.find((title) =>
-                            title.placeholder.includes("completar")
-                          )?.description ?? "",
-                      }}
-                      color="dark.3"
-                      weight={500}
-                    />
-                  )}
-                <Group>
-                  {question.options.map((option, inx) => (
-                    <OptionButton
-                      key={optionArrKey(option, inx)}
-                      isCorrect={option.isCorrect}
-                      onClick={() => setAnswer(inx)}
-                      data-selected={answer === inx}
-                    >
-                      {option.description}
-                      {option.image_url && (
-                        <Image src={option.image_url} maw="80%" mah="80%" />
-                      )}
-                    </OptionButton>
-                  ))}
-                </Group>
-              </Stack>
-            )}
+          {imageTitles.map(
+            (title) =>
+              title.file_url && (
+                <Image
+                  mx="auto"
+                  src={title.file_url}
+                  width="auto"
+                  height={190}
+                  alt={title.placeholder}
+                  key={title.file_url}
+                />
+              )
+          )}
 
-            {isTypeSelect && (
-              <Stack my="auto" align="center" spacing={hasImages ? 40 : 100}>
-                {textTitles
-                  .filter(
-                    (title) => title.description && title.description.length > 5
-                  )
-                  .map((title) => (
-                    <Title
-                      dangerouslySetInnerHTML={{
-                        __html: title.description.replace(
-                          /_+/g,
-                          singleAnswer?.description ?? "_____"
-                        ),
-                      }}
-                      color="dark.3"
-                      weight={500}
-                      size={24}
-                      key={title.description}
-                    />
-                  ))}
-                <Group>
-                  {question.options.map((option, inx) => (
-                    <OptionButton
-                      key={optionArrKey(option, inx)}
-                      isCorrect={option.isCorrect}
-                      onClick={() => setSingleAnswer(option)}
-                      data-selected={
-                        JSON.stringify(singleAnswer) === JSON.stringify(option)
-                      }
-                    >
-                      {option.description}
-                      {option.image_url && (
-                        <Image src={option.image_url} maw="80%" mah="80%" />
-                      )}
-                    </OptionButton>
-                  ))}
-                </Group>
-              </Stack>
-            )}
-          </Stack>
-        </Center>
+          {isTypeComplete && (
+            <Stack my="auto" align="center" spacing={100}>
+              {textTitles.find((title) =>
+                title.placeholder.includes("completar")
+              )?.description && (
+                  <Title
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        textTitles.find((title) =>
+                          title.placeholder.includes("completar")
+                        )?.description ?? "",
+                    }}
+                    color="dark.3"
+                    weight={500}
+                  />
+                )}
+              <Group mb={20}>
+                {question.options.map((option, inx) => (
+                  <OptionButton
+                    key={optionArrKey(option, inx)}
+                    isCorrect={option.isCorrect}
+                    onClick={() => setAnswer(inx)}
+                    data-selected={answer === inx}
+                  >
+                    {option.description}
+                    {option.image_url && (
+                      <Image src={option.image_url} maw="80%" mah="80%" />
+                    )}
+                  </OptionButton>
+                ))}
+              </Group>
+            </Stack>
+          )}
+
+          {isTypeSelect && (
+            <Stack my="auto" align="center" spacing={hasImages ? 40 : 100}>
+              {textTitles
+                .filter(
+                  (title) => title.description && title.description.length > 5
+                )
+                .map((title) => (
+                  <Title
+                    dangerouslySetInnerHTML={{
+                      __html: title.description.replace(
+                        /_+/g,
+                        singleAnswer?.description ?? "_____"
+                      ),
+                    }}
+                    color="dark.3"
+                    weight={500}
+                    size={24}
+                    key={title.description}
+                  />
+                ))}
+              <Group mb={20}>
+                {question.options.map((option, inx) => (
+                  <OptionButton
+                    key={optionArrKey(option, inx)}
+                    isCorrect={option.isCorrect}
+                    onClick={() => setSingleAnswer(option)}
+                    data-selected={
+                      JSON.stringify(singleAnswer) === JSON.stringify(option)
+                    }
+                  >
+                    {option.description}
+                    {option.image_url && (
+                      <Image src={option.image_url} maw="80%" mah="80%" />
+                    )}
+                  </OptionButton>
+                ))}
+              </Group>
+            </Stack>
+          )}
+        </Stack>
       </ScrollArea>
       <EduButton
         disabled={disabled}

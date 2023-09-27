147665c4c0c2071897f714e6c92d5ca43bb427fb
Éder Franco <eder.f@lyncas.net>
2023-09-27 11:35:18 -0300

    fix[EEE-654]

--- src/components/QuestionLoader/templates/Model32.tsx
+++ src/components/QuestionLoader/templates/Model32.tsx
@@ -127,7 +127,7 @@ export function Model32({ question, answerCallback }: ModelProps) {
           </ScrollArea>
 
           <ScrollArea h={lousaHeight * 60 / 100} px={30} type="always">
-            <Stack>
+            <Stack style={{ marginBottom: "5px" }}>
               <Text size={20} weight={600} color="dark.3" align="center">
                 {
                   textTitles.find(

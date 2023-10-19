4a6e6b43d2359e523e146b3c7082de9b8c70ba2e
bea <beatriztattooer@gmail.com>
2023-10-19 09:31:53 -0300

    :bug: fix[models]: 3

--- src/components/QuestionLoader/templates/Model3.tsx
+++ src/components/QuestionLoader/templates/Model3.tsx
@@ -1,88 +1,75 @@
 import { useEffect, useState } from "react";
 import { useQuestionHelper } from "~/hooks/useQuestionHelper";
 import { useMediaTrackStore } from "~/stores/media-track.store";
-import { usePlanetAnswer } from "~/api/planet";
-import { lousaHeight, lousaWidth } from "~/constants/dimensions";
+import { boardW } from "~/constants/dimensions";
 
 // Components:
-import { Group, Box, SimpleGrid, Image, LoadingOverlay } from "@mantine/core";
-import { EduButton } from "~/components/EduButton";
+import { Group, Box, SimpleGrid, Image, createStyles } from "@mantine/core";
 import { OptionButton } from "~/components/OptionButton";
 import { VideoPlayer } from "~/components/VideoPlayer";
 
-export function Model3({ question, answerCallback }: ModelProps) {
+const useStyles = createStyles({
+    option: {
+        width: boardW(130),
+        height: boardW(130)
+    },
+})
+
+export function Model3({ question }: ModelProps) {
+    const { classes } = useStyles();
+
     const { videoTitles } = useQuestionHelper(question);
     const mediaTrack = useMediaTrackStore();
     const [answer, setAnswer] = useState<Answer | null>(null);
 
-    const { mutate, isLoading } = usePlanetAnswer({
-        onSuccess: (q) => answerCallback(q),
-    });
-    function submitAnswer() {
-        if (answer === null) return;
-
-        mutate({
-            planetId: question.planet_id,
-            questionId: question.id,
-            optionsAnswered: [answer],
-        });
-    }
+    const [options, setOptions] = useState(null);
 
     useEffect(() => {
         setAnswer(null);
+        setOptions(null)
+        setOptions(question?.options)
     }, [question]);
 
-    console.log(question)
     return (
         <>
             <Group
                 m="auto"
-                spacing={(lousaWidth * 5 / 100)}
+                spacing={boardW(50)}
             >
-                <Box maw={lousaWidth * 10 / 100}>
+                <Box maw={boardW(400)}>
                     <VideoPlayer
                         src={videoTitles[0]?.file_url ?? ""}
                         onPlayStatusChange={mediaTrack.setPlayStatus}
                         canPlay={mediaTrack.canPlay()}
                         autoPlay
-                        customHeight={(lousaWidth * 10 / 100).toString()}
+                        style={{ height: boardW(240) }}
                     />
                 </Box>
-                <Box maw={lousaWidth * 80 / 100}>
-                    <SimpleGrid cols={4}>
-                        {question.options.map((option) => (
+                <Box maw={boardW(550)}>
+                    <SimpleGrid cols={2}>
+                        {options && options.map((option: Array<{}>) => (
                             <OptionButton
-                                key={option.description}
+                                key={option?.image_id ?? option?.description}
                                 onClick={() =>
                                     setAnswer({
-                                        position: option.position,
-                                        positionAnswer: option.position,
+                                        position: option?.position,
+                                        positionAnswer: option?.position,
                                     })
                                 }
-                                data-selected={answer?.position === option.position}
-                                isCorrect={option.isCorrect}
+                                data-selected={answer?.position === option?.position}
+                                isCorrect={option?.isCorrect}
+                                className={classes.option}
                             >
-                                <Image src={option?.image_url} />
-                                {option.description}
+                                <Image
+                                    height={boardW(120)}
+                                    width="auto"
+                                    src={option?.image_url} />
+                                {option?.description}
                             </OptionButton>
                         ))}
                     </SimpleGrid>
                 </Box>
             </Group>
-
-            {/* Continue to the next screen button */}
-            <EduButton
-                disabled={!answer}
-                onClick={submitAnswer}
-                style={{
-                    marginTop: "auto"
-                }}
-            >
-                Continuar
-            </EduButton>
-
-            {/* Loading animation */}
-            <LoadingOverlay visible={isLoading} style={{ maxHeight: lousaHeight * 80 / 100 }} />
         </>
     )
 }

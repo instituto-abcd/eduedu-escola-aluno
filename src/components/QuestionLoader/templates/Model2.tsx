a95669c326a76d6a757a2b1d0d26da0614808f80
bea <beatriztattooer@gmail.com>
2023-11-13 22:07:39 -0300

    fix[model2]

--- src/components/QuestionLoader/templates/Model2.tsx
+++ src/components/QuestionLoader/templates/Model2.tsx
@@ -1,4 +1,4 @@
-import { Group, SimpleGrid, Stack } from "@mantine/core";
+import { Group, SimpleGrid, Stack, Text } from "@mantine/core";
 import { ModelProps } from ".";
 import { DraggableCardSlot, DraggableCard } from "~/components/DraggableCard";
 import { useCallback, useEffect, useState } from "react";
@@ -28,9 +28,10 @@ export function Model2({
       })
     );
   },
-  []);
+    []);
 
-  const { audioTitles } = useQuestionHelper(question);
+  const { audioTitles, textTitles } = useQuestionHelper(question);
+  console.log('textTitles:', textTitles)
   const mediaTrack = useMediaTrackStore();
 
   useEffect(() => {
@@ -64,7 +65,19 @@ export function Model2({
 
       {/* Board content */}
       <Stack my="auto">
-        <SimpleGrid cols={question.options.length} spacing={boardW(24)}>
+
+        {textTitles.map((title) => (
+          <Text
+            size={boardW(24)}
+            color="dark.3"
+            weight={500}
+            key={title.description}
+          >
+            {title.description}
+          </Text>
+        ))}
+
+        <SimpleGrid cols={question.options.length} spacing={boardW(20)}>
           {answers.map((slot, inx) => (
             <DraggableCardSlot
               key={inx}

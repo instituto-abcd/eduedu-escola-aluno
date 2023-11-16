3581371dce2137eed0c0a545b23f8ad66d6ce88c
bea <beatriztattooer@gmail.com>
2023-11-16 08:33:14 -0300

    fix[model19]

--- src/components/QuestionLoader/templates/Model19.tsx
+++ src/components/QuestionLoader/templates/Model19.tsx
@@ -39,6 +39,13 @@ export function Model19({
     descRule === undefined ? true : descRule.value === "false" ? false : true
   );
 
+  const targetLettersRule = question.rules.find(
+    (rule) => rule.name === "show_targets_letters"
+  );
+  const showTargetLetters = Boolean(
+    targetLettersRule === undefined ? true : targetLettersRule.value === "false" ? false : true
+  );
+
   function handleDrop(item: QuestionOption, index: number) {
     setAnswers((state) =>
       produce(state, (draft) => {
@@ -89,7 +96,9 @@ export function Model19({
               key={inx}
               className={classes.slot}
             >
-              {slot}
+              {showTargetLetters &&
+                <>{slot}</>
+              }
             </DraggableCardSlot>
           ))}
         </Group>

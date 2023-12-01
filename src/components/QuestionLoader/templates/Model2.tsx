85f88eac48ce1c0a823c9e93cb3cc729142a9214
Guilherme Tabuada <ggtbh@outlook.com>
2023-12-01 10:25:00 -0300

    fix: skipDebug

--- src/components/QuestionLoader/templates/Model2.tsx
+++ src/components/QuestionLoader/templates/Model2.tsx
@@ -116,6 +116,7 @@ export function Model2({
                   sound={slot?.sound_url}
                   disabled
                   onClear={() => handleDrop(null, inx)}
+                  debug={{ skipDebug: true }}
                 />
               }
             />

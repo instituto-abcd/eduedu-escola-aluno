a05a91a91078774de0ae9e7d008a267653b598b5
Willian <willian.s@lyncas.net>
2023-12-18 16:18:13 -0300

    fix(model12): set positive feedback on all drops

--- src/components/QuestionLoader/templates/Model12.tsx
+++ src/components/QuestionLoader/templates/Model12.tsx
@@ -28,7 +28,7 @@ export function Model12({
       setStack(
         stack.filter((item) => JSON.stringify(item) !== JSON.stringify(option))
       );
-      handleFeedback("left", option);
+      handleFeedback();
     },
   });
 
@@ -39,18 +39,12 @@ export function Model12({
       setStack(
         stack.filter((item) => JSON.stringify(item) !== JSON.stringify(option))
       );
-      handleFeedback("right", option);
+      handleFeedback();
     },
   });
 
-  function handleFeedback(position: "left" | "right", option: QuestionOption) {
-    if (position === "left" && option.isCorrect === false) {
-      AudioInterface.feedback.positive.play();
-    } else if (position === "right" && option.isCorrect === true) {
-      AudioInterface.feedback.positive.play();
-    } else {
-      AudioInterface.feedback.negative.play();
-    }
+  function handleFeedback() {
+    AudioInterface.feedback.positive.play();
   }
 
   useEffect(() => {

ddb87149f6db87ce497d08e4fc45ebdd25dc3157
Willian <willian.s@lyncas.net>
2023-12-18 17:00:23 -0300

    fix(model4): increase option image height

--- src/components/QuestionLoader/templates/Model4.tsx
+++ src/components/QuestionLoader/templates/Model4.tsx
@@ -104,7 +104,7 @@ export function Model4({
                   <img
                     src={option.image_url}
                     alt={option.description}
-                    height={105}
+                    height={boardW(130)}
                     width="auto"
                     style={{
                       maxHeight: 120,

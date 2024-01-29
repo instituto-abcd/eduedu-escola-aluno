9be9ce3bbbf81115d7a98357e2b824cc8c6b0f97
Willian <willian.s@lyncas.net>
2024-01-29 09:44:35 -0300

    refactor: extract OptionButton styles

--- src/components/OptionButton/OptionButton.tsx
+++ src/components/OptionButton/OptionButton.tsx
@@ -1,56 +1,9 @@
-import { createStyles } from "@mantine/core";
-import { boardW } from "~/constants/dimensions";
 import { useDebugInfo } from "~/stores/debug-info";
 import { QuestionOption } from "~/api/exam";
 import { useCreateSound } from "~/hooks/useCreateSound";
 import { DebugDiv } from "../Debug/DebugDiv";
 import { DebugProps } from "../Debug";
-
-const useStyles = createStyles({
-  button: {
-    width: boardW(170),
-    height: boardW(148),
-    borderRadius: 8,
-    border: "1px solid #228BE6",
-    backgroundColor: "#fff",
-    cursor: "pointer",
-    boxShadow: "0px 5px 0px 0px #228BE6",
-    display: "grid",
-    placeItems: "center",
-    fontSize: boardW(20),
-    fontWeight: 600,
-    color: "#228BE6",
-    position: "relative",
-    userSelect: "none",
-    "*": {
-      color: "#228BE6",
-    },
-    ":not(:disabled):active": {
-      boxShadow: "0px 2px 0px 0px #228BE6",
-      transform: "translateY(3px)",
-    },
-    "&[data-selected=true]": {
-      backgroundColor: "#DFFEC5",
-      border: "1px solid #ACE655",
-      boxShadow: "0px 5px 0px 0px #ACE655",
-    },
-    ":disabled": {
-      backgroundColor: "#E9E9E9",
-      color: "#C4C4C4",
-      boxShadow: "0px 8px 0px 0px #c4c4c4",
-      borderColor: "#c4c4c4",
-      img: {
-        opacity: 0.5,
-      },
-      "*": {
-        color: "#C4C4C4",
-      },
-    },
-  },
-  audio: {
-    display: "none",
-  },
-});
+import { useStyles } from "./styles";
 
 export type OptionButtonProps =
   React.ButtonHTMLAttributes<HTMLButtonElement> & {

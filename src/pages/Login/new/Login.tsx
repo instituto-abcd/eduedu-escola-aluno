8be8e8a8a31b6d3e6ce905525504ab528b00f3d7
gtabuada <ggtbh@outlook.com>
2024-08-01 10:24:44 -0300

    Login role selection screen

---
+++ src/pages/Login/new/Login.tsx
@@ -0,0 +1,105 @@
+import { createStyles } from "@mantine/core";
+import { EduEduLogo } from "~/components/icons/EduEduLogo";
+
+import professor360w from "~/assets/bgs/login-professor-360w.png";
+import aluno360w from "~/assets/bgs/login-aluno-360w.png";
+import professor768w from "~/assets/bgs/login-professor-768w.png";
+import aluno768w from "~/assets/bgs/login-aluno-768w.png";
+import professor1024w from "~/assets/bgs/login-professor-1024w.png";
+import aluno1024w from "~/assets/bgs/login-aluno-1024w.png";
+
+const useStyles = createStyles(() => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100vw",
+    minHeight: "100vh",
+    userSelect: "none",
+
+    [`@media ${MEDIA_QUERY.TABLET_HORZ}`]: {
+      flexDirection: "row",
+    },
+  },
+
+  selectionBox: {
+    width: "100%",
+    height: "50%",
+    filter: "grayscale(1)",
+    transition: "filter 200ms ease-in-out",
+    position: "relative",
+    containerType: "inline-size",
+    backgroundColor: "red",
+
+    ":hover": {
+      filter: "none",
+    },
+
+    h2: {
+      width: "fit-content",
+      height: "fit-content",
+      fontSize: "12cqw",
+      fontWeight: "bold",
+      position: "absolute",
+      inset: 0,
+      top: "auto",
+      marginInline: "auto",
+      color: "white",
+      [`@media ${MEDIA_QUERY.TABLET_VERT}`]: {
+        fontSize: "8cqw",
+      },
+      [`@media ${MEDIA_QUERY.TABLET_HORZ}`]: {
+        top: 0,
+        bottom: "auto",
+      },
+    },
+
+    img: {
+      minWidth: "100vw",
+      [`@media ${MEDIA_QUERY.TABLET_HORZ}`]: {
+        minWidth: "50vw",
+      },
+    },
+  },
+
+  floatingLogo: {
+    position: "fixed",
+    bottom: "1.5rem",
+    right: "1.5rem",
+    zIndex: 5,
+    pointerEvents: "none",
+  },
+}));
+
+const MEDIA_QUERY = {
+  MOBILE: "(max-width: 360px)",
+  TABLET_VERT: "(min-width: 768px)",
+  TABLET_HORZ: "(min-width: 1024px)",
+  DESKTOP: "(min-width: 1024px)",
+};
+
+export function LoginPage() {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.selectionBox}>
+        <picture>
+          <source srcSet={aluno1024w} media={MEDIA_QUERY.TABLET_HORZ} />
+          <source srcSet={aluno768w} media={MEDIA_QUERY.TABLET_VERT} />
+          <img src={aluno360w} alt="Login: Aluno" />
+        </picture>
+        <h2>Aluno</h2>
+      </div>
+      <div className={classes.selectionBox}>
+        <picture>
+          <source srcSet={professor1024w} media={MEDIA_QUERY.TABLET_HORZ} />
+          <source srcSet={professor768w} media={MEDIA_QUERY.TABLET_VERT} />
+          <img src={professor360w} alt="Login: Professor" />
+        </picture>
+        <h2>Professor</h2>
+      </div>
+
+      <EduEduLogo className={classes.floatingLogo} />
+    </div>
+  );
+}
--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "~/pages/Login/Login";
+import { LoginPage as NewLoginPage } from "~/pages/Login/new/Login";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route index Component={LoginPage} />
+      <Route path="new">
+        <Route index Component={NewLoginPage} />
+      </Route>
     </Routes>
   );
 }

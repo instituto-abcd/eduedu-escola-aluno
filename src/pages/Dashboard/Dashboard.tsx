3d4ee4dfb067b88c5575f82c58ea16782a52f045
Felipe Paz <felipe.p@lyncas.net>
2024-10-28 09:27:41 -0300

    feature: import planet grid

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ import { useRef, useState } from "react";
 import { ViewModeToggle, type ViewMode } from "~/components/ViewModeToggle";
 import { ArrowDownBtn } from "~/components/icons/ArrowDownBtn";
 import { MEDIA_QUERY } from "~/constants/dimensions";
+import { PlanetsGrid } from "~/components/PlanetsGrid";
 
 const useStyles = createStyles({
   bg: {
@@ -73,6 +74,10 @@ export function DashboardPage() {
         >
           <PlanetTrack visible={viewMode === "planets"} ref={trackRef} />
           <AwardsGrid visible={viewMode === "awards"} />
+          <PlanetsGrid
+            visible={viewMode === "list"}
+            setViewMode={setViewMode}
+          />
         </Stack>
       </Stack>
       {viewMode === "planets" && (

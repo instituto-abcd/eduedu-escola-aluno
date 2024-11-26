311ee32ecaa7f6798d354189694f8051fb01eead
gtabuada <ggtbh@outlook.com>
2024-11-26 18:09:25 -0300

    MODEL10 expand options on tablet vertical & horizontal screens

--- src/components/QuestionLoader/templates/Model10.tsx
+++ src/components/QuestionLoader/templates/Model10.tsx
@@ -10,6 +10,8 @@ import {
   CardOption,
 } from "~/components/question-components";
 import { validString } from "~/utils/string";
+import { cx } from "~/utils/cx";
+import { useMediaQuery } from "@mantine/hooks";
 
 export function Model10({
   question,
@@ -44,66 +46,87 @@ export function Model10({
       validString(title.description) && !title.placeholder.includes("ID")
   );
 
+  // Breakpoint
+  const isLg = useMediaQuery("(min-width: 1024px)");
+
   return (
-    <div className="grow flex flex-col gap-5 size-full max-h-[80vh]">
+    <>
       {hasAudioTitle && (
         <AudioContainer question={question}>
           {auxQuestion && <ReadButton question={auxQuestion} />}
         </AudioContainer>
       )}
 
-      {imageTitles.length !== 0 &&
-        regularTextTitles.map((title, inx) => (
-          <div
-            className="text-text font-bold text-lg text-center md:text-2xl"
-            key={inx}
-            dangerouslySetInnerHTML={{ __html: title.description ?? "" }}
-          />
-        ))}
-
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-5 md:gap-9 size-full">
-        {(imageTitles.length > 0 || regularTextTitles.length > 0) && (
-          <div className="lg:w-1/2">
-            {imageTitles.length === 0 &&
-              regularTextTitles.map((title, inx) => (
-                <TextBubble
-                  key={inx}
-                  text={title.description ?? ""}
-                />
-              ))}
-
-            <ImageTitle titles={imageTitles} />
-          </div>
-        )}
-
-        <div className="grid grid-cols-2 gap-5 lg:w-[50%] lg:aspect-square content-center justify-items-stretch">
-          {question.options.map((option, inx) => (
-            <CardOption
-              className="lg:odd:ml-auto"
+      <div className="flex flex-col gap-5 size-full max-h-full">
+        {imageTitles.length !== 0 &&
+          regularTextTitles.map((title, inx) => (
+            <div
+              className="text-text font-bold text-lg text-center md:text-2xl"
               key={inx}
-              onClick={() =>
-                setAnswer({
-                  ...option,
-                  positionAnswer: question.orderedAnswer
-                    ? +option.position
-                    : undefined,
-                })
-              }
-              selected={
-                JSON.stringify(answer) ===
-                JSON.stringify({
-                  ...option,
-                  positionAnswer: question.orderedAnswer
-                    ? option.position
-                    : undefined,
-                })
-              }
-              option={option}
-              properties={[hideTextRule ? null : "text", "image", "audio"]}
+              dangerouslySetInnerHTML={{ __html: title.description ?? "" }}
             />
           ))}
+
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-5 md:gap-9 size-full grow">
+          {/* Content container */}
+          {(imageTitles.length > 0 || regularTextTitles.length > 0) && (
+            <div className="sm:size-full max-w-[700px] lg:w-1/2 flex flex-col justify-center items-center">
+              {imageTitles.length === 0 &&
+                regularTextTitles.map((title, inx) => (
+                  <div
+                    className="text-text font-bold text-lg text-center md:text-2xl"
+                    key={inx}
+                    dangerouslySetInnerHTML={{
+                      __html: title.description ?? "",
+                    }}
+                  />
+                ))}
+
+              <ImageTitle titles={imageTitles} />
+            </div>
+          )}
+
+          {/* Option container */}
+          <div
+            className={cx(
+              // BASE
+              "aspect-square w-full h-auto max-h-[400px] justify-items-center items-center grid grid-cols-2 grid-rows-2 gap-5 max-w-[700px]",
+              { ["grid-rows-3"]: question.options.length === 6 },
+              // TABLET VERT
+              "md:aspect-auto",
+              // TABLET HORZ
+              "lg:w-[50%] lg:max-h-[600px] lg:h-auto lg:aspect-square lg:justify-items-center lg:items-center"
+            )}
+          >
+            {question.options.map((option, inx) => (
+              <CardOption
+                shape={isLg ? "square" : "contain"}
+                className="lg:odd:ml-auto"
+                key={inx}
+                onClick={() =>
+                  setAnswer({
+                    ...option,
+                    positionAnswer: question.orderedAnswer
+                      ? +option.position
+                      : undefined,
+                  })
+                }
+                selected={
+                  JSON.stringify(answer) ===
+                  JSON.stringify({
+                    ...option,
+                    positionAnswer: question.orderedAnswer
+                      ? option.position
+                      : undefined,
+                  })
+                }
+                option={option}
+                properties={[hideTextRule ? null : "text", "image", "audio"]}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

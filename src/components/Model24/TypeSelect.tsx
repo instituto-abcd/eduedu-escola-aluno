2ee1c578efbba732e61e31b30866c3c980336cff
Felipe Paz <felipe.p@lyncas.net>
2024-12-11 08:36:30 -0300

    feat: enhance Model24TypeSelect with transformed text titles and FadedScrollerIndicator

--- src/components/Model24/TypeSelect.tsx
+++ src/components/Model24/TypeSelect.tsx
@@ -4,6 +4,7 @@ import { boardW } from "~/constants/dimensions";
 import { Question, QuestionOption, QuestionTitle } from "~/api/exam";
 import { IconVolume } from "@tabler/icons-react";
 import { useMemo } from "react";
+import { FadedScrollerIndicator } from "../FadedScrollerIndicator";
 
 interface TypeSelectProps {
   textTitles: QuestionTitle[];
@@ -66,82 +67,129 @@ export const Model24TypeSelect = ({
     return hasLongDescription ? "flex-col" : "grid grid-cols-2";
   }, [question.options]);
 
+  const transformedTextTitles = useMemo(() => {
+    return textTitles.reduce(
+      (
+        acc: { statements: QuestionTitle[]; rules: QuestionTitle[] },
+        cur: QuestionTitle
+      ) => {
+        if (cur?.position === 1 && cur?.description) {
+          acc.statements.push(cur);
+        }
+        if (cur?.position === 3) {
+          acc.rules.push(cur);
+        }
+
+        return acc;
+      },
+      { statements: [], rules: [] }
+    );
+  }, [textTitles]);
+
   return (
     <div className="w-full">
-      {textTitles
-        .filter((title) => title.description?.length > 0)
-        .map((title) => (
-          <Title
-            key={title.description}
-            dangerouslySetInnerHTML={{
-              __html: title.description.replace(/_+/g, dashes),
-            }}
-            size={boardW(40)}
-            weight={500}
-            color="dark.3"
-            align="center"
-          />
-        ))}
+      <div className="grid gap-4 md:grid-cols-[1fr_1fr] md:grid-rows-[auto_auto_auto]">
+        <div className="order-2 md:order-1 md:row-span-3 flex items-center justify-center">
+          <div className="w-full flex items-center justify-center">
+            {imageTitles.map(
+              (title) =>
+                title.file_url && (
+                  <Stack
+                    key={title.file_url}
+                    className={`${classes.container} lg:w-[70%] w-full`}
+                  >
+                    <Image
+                      src={title.file_url}
+                      alt={title.placeholder}
+                      styles={{
+                        image: {
+                          marginInline: "auto",
+                          objectFit: "contain",
+                        },
+                      }}
+                    />
+                  </Stack>
+                )
+            )}
+          </div>
+        </div>
 
-      <div
-        className={`flex flex-col md:flex-row items-center justify-center w-full gap-4 mt-4 ${classes.container}`}
-      >
-        {imageTitles.map(
-          (title) =>
-            title.file_url && (
-              <Stack
-                key={title.file_url}
-                className={`${classes.container} lg:w-[70%] w-full`}
-              >
-                <Image
-                  src={title.file_url}
-                  alt={title.placeholder}
-                  styles={{
-                    image: {
-                      marginInline: "auto",
-                      objectFit: "contain",
-                    },
-                  }}
-                />
-              </Stack>
-            )
-        )}
+        <div className="order-1 md:order-2 pr-4">
+          {transformedTextTitles.statements
+            .filter((title) => title.description?.length > 0)
+            .map((title) => (
+              <Title
+                key={title.description}
+                dangerouslySetInnerHTML={{
+                  __html: title.description.replace(/_+/g, dashes),
+                }}
+                size={boardW(40)}
+                weight={500}
+                color="dark.3"
+                align="center"
+              />
+            ))}
+        </div>
 
-        <div
-          className={`flex w-full ${dynamicFlexBehavior} ${
-            question.options.length === 3
-              ? classes.threeButtons
-              : classes.container
-          } items-center justify-center gap-4`}
-        >
-          {question.options.map((option, idx) => (
-            <OptionButton
-              key={idx}
-              option={option}
-              onClick={() => setSingleAnswer(option)}
-              data-selected={
-                JSON.stringify(singleAnswer) === JSON.stringify(option)
-              }
-              className={`flex-1 w-full min-h-16 max-h-32 option-group ${
-                option.description
-                  ? classes.descriptionButton
-                  : classes.optionButton
-              }`}
+        <div className="order-3 md:order-3 ">
+          {transformedTextTitles.rules
+            .filter((title) => title.description?.length > 0)
+            .map((title) => (
+              <Title
+                key={title.description}
+                dangerouslySetInnerHTML={{
+                  __html: title.description.replace(/_+/g, dashes),
+                }}
+                size={boardW(40)}
+                weight={500}
+                color="dark.3"
+                align="center"
+              />
+            ))}
+        </div>
+
+        <div className="order-4 md:order-4 pr-4">
+          <div
+            className={`flex flex-col md:flex-row items-center justify-center w-full gap-4 mt-4 ${classes.container}`}
+          >
+            <div
+              className={`flex w-full ${dynamicFlexBehavior} ${
+                question.options.length === 3
+                  ? classes.threeButtons
+                  : classes.container
+              } items-center justify-center gap-4`}
             >
-              {option.description}
-              {option.image_url && (
-                <img
-                  src={option.image_url}
-                  style={{ height: boardW(170) }}
-                />
-              )}
-              {option.sound_url && !option.image_url && (
-                <IconVolume size={boardW(62)} />
-              )}
-            </OptionButton>
-          ))}
+              {question.options.map((option, idx) => (
+                <OptionButton
+                  key={idx}
+                  option={option}
+                  onClick={() => setSingleAnswer(option)}
+                  data-selected={
+                    JSON.stringify(singleAnswer) === JSON.stringify(option)
+                  }
+                  className={`flex-1 w-full min-h-16 max-h-32 option-group ${
+                    option.description
+                      ? classes.descriptionButton
+                      : classes.optionButton
+                  }`}
+                >
+                  {option.description}
+                  {option.image_url && (
+                    <img
+                      src={option.image_url}
+                      style={{ height: boardW(170) }}
+                    />
+                  )}
+                  {option.sound_url && !option.image_url && (
+                    <IconVolume size={boardW(62)} />
+                  )}
+                </OptionButton>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
+      <FadedScrollerIndicator />
     </div>
   );
 };

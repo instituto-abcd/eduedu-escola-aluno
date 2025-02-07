139898cea0da455dbf60649fc2651c23afaa2ea0
gtabuada <ggtbh@outlook.com>
2025-02-07 08:38:16 -0300

    [MODEL24] fix breakpoint

--- src/components/Model24/TypeSelect.tsx
+++ src/components/Model24/TypeSelect.tsx
@@ -89,7 +89,7 @@ export const Model24TypeSelect = ({
 
   return (
     <div className="w-full">
-      <div className="grid gap-4 md:grid-cols-[1fr_1fr] md:grid-rows-[auto_auto_auto]">
+      <div className="grid gap-4 lg:grid-cols-[1fr_1fr] md:grid-rows-[auto_auto_auto]">
         <div className="order-2 md:order-1 md:row-span-3 flex items-center justify-center">
           <div className="w-full flex items-center justify-center">
             <ImageTitle titles={imageTitles} />
@@ -168,7 +168,6 @@ export const Model24TypeSelect = ({
           </div>
         </div>
       </div>
-      <FadedScrollerIndicator />
     </div>
   );
 };

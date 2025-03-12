00fd8c5fcc261829758b427d1691982087f2d14a
Dangocan <danilogc1999@gmail.com>
2025-03-12 15:15:44 -0300

    fix: Header de botões - Erase button

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -4,3 +4,4 @@ export { ButtonListen } from "./ButtonListen";
 export { ButtonContinue } from "./ButtonContinue";
 export { ButtonRead } from "./ButtonRead";
 export { ButtonTVPlay } from "./ButtonTVPlay";
+export { ButtonErase } from "./ButtonErase";

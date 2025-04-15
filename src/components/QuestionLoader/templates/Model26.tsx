11fe2d196dd02527e23437116efd79a23457b251
gtabuada <ggtbh@outlook.com>
2025-04-15 18:29:40 -0300

    MODEL26

--- src/components/QuestionLoader/templates/Model26.tsx
+++ src/components/QuestionLoader/templates/Model26.tsx
@@ -1,201 +1,229 @@
-import { Group, Image, Stack, Text, createStyles } from "@mantine/core";
+import {
+	DndContext,
+	type DragEndEvent,
+	DragOverlay,
+	type DragStartEvent,
+	MouseSensor,
+	TouchSensor,
+	useSensor,
+	useSensors,
+} from "@dnd-kit/core";
 import { produce } from "immer";
-import { CSSProperties, Fragment, useEffect, useState, useCallback, useMemo } from "react";
-import { QuestionOption } from "~/api/exam";
-import { AudioButton } from "~/components/AudioButton";
-import { DraggableLetters } from "~/components/DraggableLetters";
-import { DragLetterSlot } from "~/components/DraggableLetters/DragLetterSlot";
-import { boardW } from "~/constants/dimensions";
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react";
+import { v4 as uuid } from "uuid";
+import type { QuestionOption } from "~/api/exam";
+import { AudioContainer } from "~/components/AudioContainer";
+import { DraggableLetter, DroppableLetter } from "~/components/dnd";
+import { ImageTitle } from "~/components/question-components";
 import { useQuestionHelper } from "~/hooks/useQuestionHelper";
-import { ModelProps } from ".";
-
-const useStyles = createStyles({
-  letters: {
-    width: boardW(60),
-    height: boardW(40),
-    padding: 0,
-    textAlign: "center",
-    fontSize: boardW(16),
-    display: "grid",
-    placeContent: "center",
-  },
-});
+import type { ModelProps } from ".";
 
 export function Model26({
-  question,
-  onAnswerChange,
-  onConditionsChange,
+	question,
+	onAnswerChange,
+	onConditionsChange,
 }: ModelProps) {
-  const { classes } = useStyles();
-  const {
-    audioTitles,
-    hasAudioTitle,
-    audioTitleAutoplay,
-    imageTitles,
-    textTitles,
-  } = useQuestionHelper(question);
-  const letterSlots = textTitles.find((title) =>
-    title?.description?.includes("__")
-  );
+	const { hasAudioTitle, imageTitles, textTitles } =
+		useQuestionHelper(question);
+	const letterSlots = textTitles.find((title) =>
+		title?.description?.includes("__"),
+	);
 
-  const isSlotsOnly = (title: string) =>
-    title.replace(/_|\s/g, "").length === 0;
+	const isSlotsOnly = (title: string) =>
+		title.replace(/_|\s/g, "").length === 0;
 
-  const initialSlots =
-    letterSlots && isSlotsOnly(letterSlots.description)
-      ? letterSlots.description.split(" ").map(() => null)
-      : [];
+	const initialSlots =
+		letterSlots && isSlotsOnly(letterSlots.description)
+			? letterSlots.description.split(" ").map(() => null)
+			: [];
 
-  const [slots, setSlots] =
-    useState<Array<QuestionOption | null>>(initialSlots);
+	const [slots, setSlots] =
+		useState<Array<QuestionOption | null>>(initialSlots);
 
-  const handleDrop = useCallback(function (item: QuestionOption | null, index: number) {
-    setSlots((state) =>
-      produce(state, (draft) => {
-        draft[index] = item;
-      })
-    );
-  }, []);
+	const handleDrop = useCallback(
+		(item: QuestionOption | null, index: number) => {
+			setSlots((state) =>
+				produce(state, (draft) => {
+					draft[index] = item;
+				}),
+			);
+		},
+		[],
+	);
 
-  function handleClear(index: number) {
-    handleDrop(null, index);
-  }
+	function handleClear(index: number) {
+		handleDrop(null, index);
+	}
 
-  useEffect(() => {
-    setSlots(initialSlots);
-  }, [question]);
+	useEffect(() => {
+		setSlots(initialSlots);
+	}, [question]);
 
-  useEffect(() => {
-    onAnswerChange(slots.filter((slot) => slot !== null).map((item, index) => ({
-      ...item, positionAnswer: index
-    })) as QuestionOption[]);
-  }, [slots]);
+	useEffect(() => {
+		onAnswerChange(
+			slots
+				.filter((slot) => slot !== null)
+				.map((item, index) => ({
+					...item,
+					positionAnswer: index,
+				})) as QuestionOption[],
+		);
+	}, [slots]);
 
-  const conditions = useMemo(
-    () => [slots.length === question.options.length && !slots.includes(null)], [slots]
-  );
+	const conditions = useMemo(
+		() => [slots.length === question.options.length && !slots.includes(null)],
+		[slots],
+	);
 
-  useEffect(() => {
-    onConditionsChange(conditions);
-  }, [slots]);
+	useEffect(() => {
+		onConditionsChange(conditions);
+	}, [slots]);
 
-  return (
-    <>
-      {hasAudioTitle && (
-        <Group mx="auto" h="50px">
-          {audioTitles.map((title, inx) => (
-            <AudioButton
-              index={inx}
-              src={title.file_url ?? ""}
-              autoPlay={audioTitleAutoplay(inx)}
-              key={title.file_url}
-            />
-          ))}
-        </Group>
-      )}
+	/* Drag Handlers */
+	const [activeDrag, setActiveDrag] = useState<QuestionOption | null>(null);
 
-      <Stack my="auto" align="center">
-        {imageTitles[0] && (
-          <Image
-            src={imageTitles[0].file_url ?? ""}
-            width="auto"
-            height={boardW(200)}
-          />
-        )}
-        <Stack align="center" spacing={boardW(20)} my="xl">
-          {letterSlots && isSlotsOnly(letterSlots.description) && (
-            <Group>
-              {slots.map((slot, inx) => (
-                <DragLetterSlot
-                  onDrop={(item) => handleDrop(item, inx)}
-                  option={slot}
-                  onClear={() => handleClear(inx)}
-                  key={inx}
-                  className={classes.letters}
-                  style={{
-                    fontSize: boardW(16),
-                  }}
-                />
-              ))}
-            </Group>
-          )}
+	function onDragStart(e: DragStartEvent) {
+		if (e.active.data.current) {
+			const option = e.active.data.current.option;
+			setActiveDrag(() => option);
+		}
+	}
 
-          {letterSlots && !isSlotsOnly(letterSlots.description) && (
-            <Group spacing={0}>
-              {letterSlots.description
-                .replaceAll(/\\n/g, " ")
-                .split(/_+/g) // separa os segmentos de texto dos underlines
-                .map((w, inx, arr) => {
-                  const notLastFragment = arr.length !== inx + 1;
-                  const isLastFragment = arr.length === 1 && w.endsWith(" ");
-                  const isFirstFragment = arr.length === 1 && w.startsWith(" ");
+	function onDragEnd(e: DragEndEvent) {
+		setActiveDrag(null);
+		if (e.over) {
+			const targetIndex = Number(e.over.id);
+			const option = (e.active.data.current?.option as QuestionOption) ?? null;
+			handleDrop(option, targetIndex);
+		}
+	}
 
-                  const canRenderLast =
-                    (isLastFragment || notLastFragment) && !isFirstFragment;
-                  const canRenderFirst = isFirstFragment && !isLastFragment;
+	const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
 
-                  const slotLetterStyle: CSSProperties = {
-                    fontSize: boardW(16),
-                  };
+	/* Map id to options */
+	const options = useMemo(
+		() =>
+			question.options.map((option) => ({
+				...option,
+				id: uuid(),
+			})),
+		[question],
+	);
 
-                  return (
-                    <Fragment key={inx}>
-                      {canRenderFirst && (
-                        <DragLetterSlot
-                          onDrop={(item) => handleDrop(item, inx)}
-                          option={slots[inx] ?? null}
-                          onClear={() => handleClear(inx)}
-                          className={classes.letters}
-                          style={slotLetterStyle}
-                        />
-                      )}
-                      {w.split(" ").map((frag, inx) => (
-                        <Text
-                          color="dark.3"
-                          size={boardW(18)}
-                          weight={700}
-                          p={0}
-                          my={2.5}
-                          mx={2.5}
-                          key={inx}
-                        >
-                          {frag}
-                        </Text>
-                      ))}
+	return (
+		<DndContext
+			onDragStart={onDragStart}
+			onDragEnd={onDragEnd}
+			sensors={sensors}
+		>
+			{hasAudioTitle && <AudioContainer question={question} />}
 
-                      {canRenderLast && (
-                        <DragLetterSlot
-                          onDrop={(item) => handleDrop(item, inx)}
-                          option={slots[inx] ?? null}
-                          onClear={() => handleClear(inx)}
-                          className={classes.letters}
-                          style={slotLetterStyle}
-                        />
-                      )}
-                    </Fragment>
-                  );
-                })}
-            </Group>
-          )}
+			<div className="size-full flex flex-col lg:flex-row lg:gap-8 items-center justify-evenly">
+				<ImageTitle titles={imageTitles} />
 
-          <Group>
-            {question.options.map((option) => (
-              <DraggableLetters
-                option={option}
-                key={option.description}
-                hidden={
-                  !!slots.find(
-                    (item) =>
-                      JSON.stringify(item) === JSON.stringify(option) && item
-                  )
-                }
-                className={classes.letters}
-                debug={{ debugProperty: "position", outside: true }}
-              />
-            ))}
-          </Group>
-        </Stack>
-      </Stack>
-    </>
-  );
+				<div className="flex flex-col items-center gap-6 max-w-screen-md">
+					{letterSlots && isSlotsOnly(letterSlots.description) && (
+						<div className="flex w-full gap-4 items-center justify-center">
+							{slots.map((_, inx) => (
+								<DroppableLetter
+									key={inx}
+									id={inx}
+									replaceWith={
+										slots[inx] && (
+											<DraggableLetter
+												id={inx}
+												optionItem={slots[inx]}
+												disabled
+												onClear={() => handleClear(inx)}
+												className="w-auto"
+											/>
+										)
+									}
+								/>
+							))}
+						</div>
+					)}
+
+					{letterSlots && !isSlotsOnly(letterSlots.description) && (
+						<div className="flex flex-wrap items-center justify-center">
+							{letterSlots.description
+								.replaceAll(/\\n/g, " ")
+								.split(/_+/g) // separa os segmentos de texto dos underlines
+								.map((w, inx, arr) => {
+									const notLastFragment = arr.length !== inx + 1;
+									const isLastFragment = arr.length === 1 && w.endsWith(" ");
+									const isFirstFragment = arr.length === 1 && w.startsWith(" ");
+
+									const canRenderLast =
+										(isLastFragment || notLastFragment) && !isFirstFragment;
+									const canRenderFirst = isFirstFragment && !isLastFragment;
+
+									return (
+										<Fragment key={inx}>
+											{canRenderFirst && (
+												<DroppableLetter
+													id={inx}
+													replaceWith={
+														slots[inx] && (
+															<DraggableLetter
+																id={inx}
+																optionItem={slots[inx]}
+																disabled
+																onClear={() => handleClear(inx)}
+																className="w-auto"
+															/>
+														)
+													}
+												/>
+											)}
+
+											{w.split(" ").map((frag, inx) => (
+												<span
+													className="text-text px-0.5 font-black text-3xl"
+													key={inx}
+												>
+													{frag}
+												</span>
+											))}
+
+											{canRenderLast && (
+												<DroppableLetter
+													id={inx}
+													replaceWith={
+														slots[inx] && (
+															<DraggableLetter
+																id={inx}
+																optionItem={slots[inx]}
+																disabled
+																onClear={() => handleClear(inx)}
+																className="w-auto"
+															/>
+														)
+													}
+												/>
+											)}
+										</Fragment>
+									);
+								})}
+						</div>
+					)}
+
+					<div className="flex gap-4">
+						{options.map((option) => (
+							<DraggableLetter
+								id={option.id}
+								optionItem={option}
+								key={option.description}
+								hidden={slots.includes(option)}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+
+			<DragOverlay>
+				{activeDrag && <DraggableLetter optionItem={activeDrag} id={542321} />}
+			</DragOverlay>
+		</DndContext>
+	);
 }

ca2d1db38e0fff6765537ed96f8d1009260f17d5
Dangocan <danilogc1999@gmail.com>
2025-04-30 17:44:12 -0300

    EEP-187: Ajuste tela inicial antes da prova

--- src/pages/Login/AudioSettings.tsx
+++ src/pages/Login/AudioSettings.tsx
@@ -7,46 +7,7 @@ import { MEDIA_QUERY } from "~/constants/dimensions";
 import { useEffect, useState } from "react";
 import { Sprite } from "~/components/vector/Sprite";
 import { Header } from "./components/Header";
-
-const useStyles = createStyles({
-  container: {
-    width: "100vw",
-    height: "100vh",
-    overflow: "clip",
-    position: "relative",
-  },
-
-  video: {
-    minWidth: "100%",
-    height: "auto",
-    maxHeight: "100%",
-    minHeight: "100vh",
-    objectPosition: "bottom",
-    objectFit: "cover",
-    position: "absolute",
-    bottom: 0,
-  },
-
-  icons: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "absolute",
-    width: "100%",
-    top: 0,
-    insetInline: 0,
-    marginInline: "auto",
-    zIndex: 55,
-    height: 100,
-    paddingInline: 40,
-    marginTop: 60,
-
-    svg: {
-      width: "auto",
-      height: "100%",
-    },
-  },
-});
+import bg from "~/assets/bg-planet-track.png";
 
 type Props = { onNext: () => void; onBack: () => void };
 
@@ -67,8 +28,8 @@ export function AudioSettings({ onNext, onBack }: Props) {
         window.innerWidth < 768
           ? "MOBILE"
           : window.innerWidth < 1024
-            ? "TABLET_VERT"
-            : "DESKTOP",
+          ? "TABLET_VERT"
+          : "DESKTOP"
       );
     };
 
@@ -81,20 +42,28 @@ export function AudioSettings({ onNext, onBack }: Props) {
     };
   }, []);
 
-  const { classes } = useStyles();
-
   return (
-    <div className={classes.container}>
-      <Header title="Escute o vídeo e siga a instrução" onClose={onBack} />
+    <div
+      className="w-screen min-h-screen flex flex-col items-center justify-between pb-10"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="w-full flex flex-col items-center">
+        <Header
+          title="Escute o vídeo e siga a instrução"
+          onClose={onBack}
+        />
 
-      <div className={classes.icons}>
-        <Sprite id={0} />
-        <Sprite id={7} onClick={onNext} />
-        <Sprite id={8} />
+        <div className="flex justify-between items-center w-full mx-auto z-[55] h-[100px] px-[40px] my-[30px] [&_svg]:w-auto [&_svg]:h-full">
+          <Sprite id={0} />
+          <Sprite
+            id={7}
+            onClick={onNext}
+          />
+          <Sprite id={8} />
+        </div>
       </div>
-
       <video
-        className={classes.video}
+        className="w-[70%]"
         src={videosDict[dimension]}
         autoPlay
         loop

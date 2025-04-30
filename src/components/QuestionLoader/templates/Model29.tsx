094e98ae32e5fc80acdff1828ca852bd8c714559
gtabuada <ggtbh@outlook.com>
2025-04-30 17:56:06 -0300

    [MODEL29] fix targets/draggables responsiveness

--- src/components/QuestionLoader/templates/Model29.tsx
+++ src/components/QuestionLoader/templates/Model29.tsx
@@ -7,19 +7,19 @@ import type { ModelProps } from ".";
 import { TextTitle } from "~/components/question-components/TextTitle";
 import { AudioContainer } from "~/components/AudioContainer";
 import {
-	DraggableCardSquare,
-	DroppableCard,
-} from "~/components/dnd";
-import {
 	DndContext,
 	type DragEndEvent,
 	DragOverlay,
 	type DragStartEvent,
 	MouseSensor,
 	TouchSensor,
+	useDraggable,
+	useDroppable,
 	useSensor,
 	useSensors,
 } from "@dnd-kit/core";
+import { cx } from "~/utils/cx";
+import { IconX } from "@tabler/icons-react";
 
 type OptionWithId = QuestionOption & { id: string };
 
@@ -113,69 +113,154 @@ export function Model29({
 		>
 			{hasAudioTitle && <AudioContainer question={question} />}
 
-			<div className="flex flex-col lg:flex-row justify-evenly items-center size-full max-h-[90vh] my-auto gap-4">
-
+			<div className="flex flex-col lg:flex-row justify-evenly items-center size-full max-h-[90vh] max-w-screen-xl my-auto gap-4">
 				<div>
 					{textTitles[0]?.description && (
 						<TextTitle text={textTitles[0].description} />
 					)}
 				</div>
 
-				<div className="flex flex-col items-center justify-between size-full gap-4 lg:h-auto lg:gap-8">
-          <div className="grid grid-cols-4 gap-4 max-w-screen-md w-full">
-					{answers.map((answer, inx) => (
-						<DroppableCard
-							key={inx}
-							id={inx}
-							className="bg-[#f8f6f2] flex items-center justify-center w-[20vw] md:w-auto shadow-card"
-							replaceWith={
-								answer && (
-									<DraggableCardSquare
-										id={(answer as QuestionOption & { id: string }).id}
-										optionItem={answer}
-										image={answers[inx]?.image_url}
-										text={answers[inx]?.description}
-										disabled
-										className="w-[20vw] md:w-auto aspect-auto max-w-none max-h-none md:max-w-none md:max-h-none"
-										onClear={() => handleClear(inx)}
-										debug={{ skipDebug: true }}
-									/>
-								)
-							}
-						>
-							<p className="text-[#DEDEDE] font-black text-5xl">{inx + 1}</p>
-						</DroppableCard>
-					))}
-				</div>
+				<div className="flex flex-col items-center justify-center size-full gap-4 lg:h-auto lg:gap-8 max-w-[550px]">
+					<div className="grid grid-cols-4 gap-2 max-w-screen-lg w-full min-h-[190px] lg:min-h-0">
+						{answers.map((answer, inx) => (
+							<Droppable
+								key={inx}
+								id={inx}
+								replaceWith={
+									answer && (
+										<Draggable
+											id={(answer as QuestionOption & { id: string }).id}
+											optionItem={answer}
+											disabled
+											onClear={() => handleClear(inx)}
+										/>
+									)
+								}
+							>
+								<p className="text-[#DEDEDE] font-black text-5xl">{inx + 1}</p>
+							</Droppable>
+						))}
+					</div>
 
-				<div className="grid grid-cols-2 size-full gap-4 max-w-screen-md max-h-[340px]">
-					{options.map((option) => (
-						<DraggableCardSquare
-							id={option.id}
-							key={option.id}
-							optionItem={option}
-							image={option.image_url}
-							text={option.description}
-							className="!max-w-none !max-h-none aspect-auto"
-							hidden={answers.some((ans) => ans?.id === option.id)}
-							debug={{ debugProperty: "position" }}
-						/>
-					))}
-				</div></div>
+					<div className="grid grid-cols-2 size-full gap-4 max-h-[280px]">
+						{options.map((option) => (
+							<Draggable
+								id={option.id}
+								key={option.id}
+								optionItem={option}
+								hidden={answers.some((ans) => ans?.id === option.id)}
+							/>
+						))}
+					</div>
+				</div>
 			</div>
 
 			<DragOverlay>
 				{activeDrag && (
-					<DraggableCardSquare
-						optionItem={activeDrag}
-						image={activeDrag.image_url}
-						text={activeDrag.description}
-						className="size-full max-w-none max-h-none aspect-auto"
-						debug={{ debugProperty: "position" }}
-						id={uuid()}
-					/>
+					<Draggable optionItem={activeDrag} id={uuid()} overlay />
 				)}
 			</DragOverlay>
 		</DndContext>
+	);
+}
+
+function Droppable({
+	id,
+	replaceWith,
+	...props
+}: {
+	replaceWith?: React.ReactNode;
+	id: number;
+	children?: React.ReactNode;
+}) {
+	const { setNodeRef, isOver } = useDroppable({ id });
+
+	if (replaceWith) return replaceWith;
+
+	return (
+		<div
+			className={cx(
+				"rounded-[20px]  transition-all bg-[#f8f6f2] flex items-center justify-center ",
+				"size-full shadow-card lg:aspect-square",
+				{
+					"bg-green-300/20": isOver,
+				},
+			)}
+			ref={setNodeRef}
+			{...props}
+		/>
+	);
+}
+
+function Draggable({
+	optionItem,
+	onClear,
+	hidden = false,
+	disabled = false,
+	id,
+	overlay = false,
+}: {
+	optionItem: QuestionOption;
+	onClear?: () => void;
+	hidden?: boolean;
+	disabled?: boolean;
+	id: string | number;
+	overlay?: boolean;
+}) {
+	const { setNodeRef, isDragging, attributes, listeners } = useDraggable({
+		id,
+		data: { option: optionItem },
+		disabled,
+	});
+
+	return (
+		<div
+			className={cx(
+				"rounded-[20px] bg-[#F8F6F2] shadow-card grid place-items-center relative",
+				"cursor-grab p-1 flex-1",
+				"w-auto h-[130px]",
+				{
+					"opacity-40 cursor-grabbing": isDragging,
+					"pointer-events-none": disabled || hidden,
+					"opacity-10": hidden,
+					"h-full": disabled,
+					"aspect-square": overlay,
+				},
+			)}
+			ref={setNodeRef}
+			{...attributes}
+			{...listeners}
+		>
+			<p
+				className={cx(
+					"font-bold text-text select-none pointer-events-none text-3xl",
+					{
+						"text-lg md:text-3xl": disabled,
+					},
+				)}
+			>
+				{optionItem.description}
+			</p>
+			<img
+				src={optionItem.image_url ?? ""}
+				alt={optionItem.description}
+				className={cx(
+					"pointer-events-none select-none object-cover my-auto aspect-square w-[80px]",
+				)}
+			/>
+			{onClear && (
+				<button
+					type="button"
+					className={cx(
+						"bg-red-500 text-white rounded-full grid place-items-center absolute",
+						"size-8 -top-3 right-auto -left-3 pointer-events-auto",
+					)}
+					onClick={onClear}
+					onKeyDown={onClear}
+				>
+					<IconX className="size-5 xl:size-7" />
+				</button>
+			)}
+		</div>
 	);
 }

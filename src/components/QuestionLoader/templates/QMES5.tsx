0efac0b673465a6165719d4922c383dd7bafcfd1
gtabuada <ggtbh@outlook.com>
2025-05-19 13:58:35 -0300

    QMES5

--- src/components/QuestionLoader/templates/QMES5.tsx
+++ src/components/QuestionLoader/templates/QMES5.tsx
@@ -1,92 +1,65 @@
-import { Box, Group, ScrollArea, SimpleGrid } from "@mantine/core";
 import { useEffect, useMemo, useState } from "react";
-import { QuestionOption } from "~/api/exam";
 import { OptionButton } from "~/components/OptionButton";
-import { VideoPlayer } from "~/components/VideoPlayer";
-import { boardW } from "~/constants/dimensions";
 import { useQuestionHelper } from "~/hooks/useQuestionHelper";
-import { ModelProps } from ".";
+import type { ModelProps } from ".";
+import type { QuestionOption } from "~/api/exam";
+import { VideoTitle } from "~/components/question-components";
 
 export function QMES5({
-  question,
-  onAnswerChange,
-  onConditionsChange,
+	question,
+	onAnswerChange,
+	onConditionsChange,
 }: ModelProps) {
-  const [selected, setSelected] = useState<QuestionOption[]>([]);
+	const [selected, setSelected] = useState<QuestionOption[]>([]);
 
-  function selectItem(answer: QuestionOption) {
-    if (selected.find((item) => item.position === answer.position)) {
-      setSelected(selected.filter((item) => item.position !== answer.position));
-    } else {
-      setSelected([
-        ...selected,
-        {
-          ...answer,
-          positionAnswer: answer.position,
-        } as QuestionOption,
-      ]);
-    }
-  }
+	function selectItem(answer: QuestionOption) {
+		if (selected.find((item) => item.position === answer.position)) {
+			setSelected(selected.filter((item) => item.position !== answer.position));
+		} else {
+			setSelected([
+				...selected,
+				{
+					...answer,
+					positionAnswer: answer.position,
+				} as QuestionOption,
+			]);
+		}
+	}
 
-  const { videoTitles, optionArrKey } = useQuestionHelper(question);
+	const { videoTitles, optionArrKey } = useQuestionHelper(question);
 
-  useEffect(() => {
-    setSelected([]);
-  }, [question]);
+	useEffect(() => {
+		setSelected([]);
+	}, [question]);
 
-  useEffect(() => {
-    onAnswerChange(selected);
-  }, [selected]);
+	useEffect(() => {
+		onAnswerChange(selected);
+	}, [selected]);
 
-  const conditions = useMemo(() => [selected.length > 0], [selected]);
+	const conditions = useMemo(() => [selected.length > 0], [selected]);
 
-  useEffect(() => {
-    onConditionsChange(conditions);
-  }, [conditions]);
+	useEffect(() => {
+		onConditionsChange(conditions);
+	}, [conditions]);
 
-  return (
-    <>
-      <Group
-        w="100%"
-        m="auto"
-      >
-        <Box
-          h="100%"
-          w="100%"
-          maw={boardW(440)}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <Box m="auto">
-            <VideoPlayer
-              src={videoTitles[0]?.file_url ?? ""}
-              autoPlay
-            />
-          </Box>
-        </Box>
+	return (
+		<div className="size-full flex flex-col lg:flex-row items-center justify-evenly">
+			<VideoTitle titles={videoTitles} autoPlay />
 
-        <ScrollArea style={{ minWidth: "30%", minHeight: "30%" }}>
-          <SimpleGrid
-            cols={2}
-            spacing={20}
-            style={{
-              marginBottom: "5px",
-            }}
-          >
-            {question.options.map((option, inx) => (
-              <OptionButton
-                key={optionArrKey(option, inx)}
-                onClick={() => selectItem(option)}
-                data-selected={
-                  !!selected.find((item) => item.position === option.position)
-                }
-                option={option}
-              >
-                {option.description}
-              </OptionButton>
-            ))}
-          </SimpleGrid>
-        </ScrollArea>
-      </Group>
-    </>
-  );
+			<div className="grid grid-cols-2 max-w-screen-md gap-4 min-w-[292px]">
+				{question.options.map((option, inx) => (
+					<OptionButton
+						key={optionArrKey(option, inx)}
+						onClick={() => selectItem(option)}
+						data-selected={
+							!!selected.find((item) => item.position === option.position)
+						}
+						option={option}
+					>
+						{option.description}
+					</OptionButton>
+				))}
+			</div>
+		</div>
+	);
 }

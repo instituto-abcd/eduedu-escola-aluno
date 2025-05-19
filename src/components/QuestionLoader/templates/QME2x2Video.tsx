b6caa10ffb6ba81492462099c4d4b7bfff946df8
gtabuada <ggtbh@outlook.com>
2025-05-19 13:51:16 -0300

    QME2x2Video

--- src/components/QuestionLoader/templates/QME2x2Video.tsx
+++ src/components/QuestionLoader/templates/QME2x2Video.tsx
@@ -1,75 +1,59 @@
-import { Box, Group, Image, SimpleGrid, Text } from "@mantine/core";
 import { IconVolume } from "@tabler/icons-react";
 import { useEffect, useMemo, useState } from "react";
-import { QuestionOption } from "~/api/exam";
 import { OptionButton } from "~/components/OptionButton";
-import { VideoPlayer } from "~/components/VideoPlayer";
-import { boardW } from "~/constants/dimensions";
 import { useQuestionHelper } from "~/hooks/useQuestionHelper";
-import { ModelProps } from ".";
+import { VideoTitle } from "~/components/question-components";
+import type { ModelProps } from ".";
+import type { QuestionOption } from "~/api/exam";
 
 export function QME2x2Video({
-  question,
-  onAnswerChange,
-  onConditionsChange,
+	question,
+	onAnswerChange,
+	onConditionsChange,
 }: ModelProps) {
-  const [answer, setAnswer] = useState<QuestionOption | null>(null);
-  const { videoTitles } = useQuestionHelper(question);
+	const [answer, setAnswer] = useState<QuestionOption | null>(null);
+	const { videoTitles } = useQuestionHelper(question);
 
-  useEffect(() => {
-    setAnswer(null);
-  }, [question]);
+	useEffect(() => {
+		setAnswer(null);
+	}, [question]);
 
-  useEffect(() => {
-    onAnswerChange(answer ? [answer] : []);
-  }, [answer]);
+	useEffect(() => {
+		onAnswerChange(answer ? [answer] : []);
+	}, [answer]);
 
-  const conditions = useMemo(() => [Boolean(answer)], [answer]);
+	const conditions = useMemo(() => [Boolean(answer)], [answer]);
 
-  useEffect(() => {
-    onConditionsChange(conditions);
-  }, [conditions]);
+	useEffect(() => {
+		onConditionsChange(conditions);
+	}, [conditions]);
 
-  return (
-    <>
-      <div className="flex flex-nowrap w-full h-full items-center justify-center">
-        <Box w="45%">
-          <VideoPlayer
-            src={videoTitles[0]?.file_url ?? ""}
-            autoPlay
-            style={{ height: boardW(250) }}
-          />
-        </Box>
+	return (
+		<div className="size-full flex flex-col lg:flex-row items-center justify-evenly">
+			<VideoTitle titles={videoTitles} autoPlay />
 
-        <SimpleGrid
-          cols={2}
-          style={{ placeItems: "center", marginBottom: "5px" }}
-          w="45%"
-        >
-          {question.options.map((option) => (
-            <OptionButton
-              key={option.position}
-              data-selected={JSON.stringify(answer) === JSON.stringify(option)}
-              onClick={() => setAnswer(option)}
-              option={option}
-            >
-              {option.image_url && (
-                <Image
-                  src={option.image_url}
-                  alt={option.description}
-                  width="100%"
-                />
-              )}
-              {!option.image_url && option.sound_url && (
-                <IconVolume size={80} />
-              )}
-              {!option.image_url && !option.sound_url && option.description && (
-                <Text>{option.description}</Text>
-              )}
-            </OptionButton>
-          ))}
-        </SimpleGrid>
-      </div>
-    </>
-  );
+			<div className="grid grid-cols-2 max-w-screen-md gap-4 min-w-[292px]">
+				{question.options.map((option) => (
+					<OptionButton
+						key={option.position}
+						data-selected={JSON.stringify(answer) === JSON.stringify(option)}
+						onClick={() => setAnswer(option)}
+						option={option}
+					>
+						{option.image_url && (
+							<img
+								src={option.image_url}
+								alt={option.description}
+								className="w-full object-cover"
+							/>
+						)}
+						{!option.image_url && option.sound_url && <IconVolume size={80} />}
+						{!option.image_url && !option.sound_url && option.description && (
+							<p className="text-text font-black">{option.description}</p>
+						)}
+					</OptionButton>
+				))}
+			</div>
+		</div>
+	);
 }

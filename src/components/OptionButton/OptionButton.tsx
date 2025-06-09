a9ae4ab24d4b35a9d49f577771ab5319f73b7f3a
gtabuada <ggtbh@outlook.com>
2025-06-09 12:14:29 -0300

    [MODEL32] melhorias visuais

--- src/components/OptionButton/OptionButton.tsx
+++ src/components/OptionButton/OptionButton.tsx
@@ -1,69 +1,58 @@
 import { useDebugInfo } from "~/stores/debug-info";
-import { QuestionOption } from "~/api/exam";
+import type { QuestionOption } from "~/api/exam";
 import { useCreateSound } from "~/hooks/useCreateSound";
 import { DebugDiv } from "../Debug/DebugDiv";
-import { DebugProps } from "../Debug";
+import type { DebugProps } from "../Debug";
 import { cx } from "~/utils/cx";
 
 export type OptionButtonProps =
-  React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    option?: QuestionOption;
-    debug?: DebugProps;
-    aspectSquare?: boolean;
-    fullHeight?: boolean;
-    classHeight?: string;
-  };
+	React.ButtonHTMLAttributes<HTMLButtonElement> & {
+		option?: QuestionOption;
+		debug?: DebugProps;
+	};
 
 export function OptionButton({
-  option,
-  debug,
-  children,
-  aspectSquare = true,
-  fullHeight = true,
-  classHeight = "h-[120px]",
-  ...props
+	option,
+	debug,
+	children,
+	...props
 }: OptionButtonProps) {
-  const { sound, isPlaying } = useCreateSound({
-    src: option?.sound_url ?? "",
-    skipPlayStatus: true,
-  });
+	const { sound, isPlaying } = useCreateSound({
+		src: option?.sound_url ?? "",
+		skipPlayStatus: true,
+	});
 
-  function onClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    if (!props.disabled && !isPlaying) {
-      sound.play();
-    }
-    props?.onClick?.(e);
-  }
+	function onClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+		if (!props.disabled && !isPlaying) {
+			sound.play();
+		}
+		props?.onClick?.(e);
+	}
 
-  const canDebug = useDebugInfo((s) => s.answer);
+	const canDebug = useDebugInfo((s) => s.answer);
 
-  // TODO: experiment with using imgUrl instead of children
-  // TODO: experiment with using text string instead of children
+	// TODO: experiment with using imgUrl instead of children
+	// TODO: experiment with using text string instead of children
 
-  return (
-    <button
-      {...props}
-      className={cx(
-        "shadow-card relative bg-surface rounded-[45px] flex flex-col items-center justify-center cursor-pointer select-none",
-        "[&:not(:disabled):active]:shadow-card-thin [&:not(:disabled):active]:translate-y-[3px]",
-        "data-[selected=true]:bg-[#DFFEC5] data-[selected=true]:border border-[#ACE655] data-[selected=true]:shadow-[0px_5px_0px_0px_#ACE655]",
-        "w-[138px] lg:w-full overflow-hidden",
-        "text-text font-extrabold text-xl lg:text-2xl xl:text-[2.5cqw] xl:leading-[100%] xl:break-words",
-        {
-          ["lg:aspect-square"]: aspectSquare === true,
-          ["lg:h-full"]: fullHeight === true,
-        },
-        classHeight,
-        props.className
-      )}
-      onClick={onClick}
-      style={{ containerType: "inline-size" }}
-      disabled={props.disabled || isPlaying}
-    >
-      {canDebug && !debug?.skipDebug && option && (
-        <DebugDiv debug={debug}>{option.isCorrect}</DebugDiv>
-      )}
-      {children}
-    </button>
-  );
+	return (
+		<button
+			{...props}
+			className={cx(
+				"shadow-card relative bg-surface rounded-[45px] flex flex-col items-center justify-center cursor-pointer select-none",
+				"[&:not(:disabled):active]:shadow-card-thin [&:not(:disabled):active]:translate-y-[3px]",
+				"data-[selected=true]:bg-[#DFFEC5] data-[selected=true]:border border-[#ACE655] data-[selected=true]:shadow-[0px_5px_0px_0px_#ACE655]",
+				"w-[138px] lg:w-full overflow-hidden",
+				"text-text font-extrabold text-xl lg:text-2xl xl:text-[2.5cqw] xl:leading-[100%] xl:break-words",
+				props.className,
+			)}
+			onClick={onClick}
+			style={{ containerType: "inline-size" }}
+			disabled={props.disabled || isPlaying}
+		>
+			{canDebug && !debug?.skipDebug && option && (
+				<DebugDiv debug={debug}>{option.isCorrect}</DebugDiv>
+			)}
+			{children}
+		</button>
+	);
 }
--- src/pages/Debug/Models/ModelView.tsx
+++ src/pages/Debug/Models/ModelView.tsx
@@ -6,81 +6,81 @@ import { QuestionInfo } from "../components/QuestionInfo";
 import { QuestionLoader } from "~/components/QuestionLoader";
 
 export function ModelView() {
-  const { modelId } = useParams();
-  const [query, setQuery] = useSearchParams();
-  const searchIndex = query.get("index");
+	const { modelId } = useParams();
+	const [query, setQuery] = useSearchParams();
+	const searchIndex = query.get("index");
 
-  const [currentQuestion, setCurrentQuestion] = useState(-1);
-  const { data } = useDebugModelQuestions(modelId ?? "", {
-    enabled: !!modelId,
-    initialData: [],
-    onSuccess: (data) => {
-      if (data.length > 0) {
-        if (
-          searchIndex === null ||
-          !Number.isInteger(+searchIndex) ||
-          +searchIndex < 0 ||
-          +searchIndex >= data.length
-        ) {
-          return setCurrentQuestion(0);
-        } else {
-          return setCurrentQuestion(parseInt(searchIndex));
-        }
-      }
-    },
-  });
+	const [currentQuestion, setCurrentQuestion] = useState(-1);
+	const { data } = useDebugModelQuestions(modelId ?? "", {
+		enabled: !!modelId,
+		initialData: [],
+		onSuccess: (data) => {
+			if (data.length > 0) {
+				if (
+					searchIndex === null ||
+					!Number.isInteger(+searchIndex) ||
+					+searchIndex < 0 ||
+					+searchIndex >= data.length
+				) {
+					return setCurrentQuestion(0);
+				} else {
+					return setCurrentQuestion(parseInt(searchIndex));
+				}
+			}
+		},
+	});
 
-  function handleNextQuestion() {
-    if (!data) return;
-    if (currentQuestion + 1 < data.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-  }
+	function handleNextQuestion() {
+		if (!data) return;
+		if (currentQuestion + 1 < data.length) {
+			setCurrentQuestion(currentQuestion + 1);
+		}
+	}
 
-  function handlePreviousQuestion() {
-    if (!data) return;
-    if (currentQuestion - 1 >= 0) {
-      setCurrentQuestion(currentQuestion - 1);
-    }
-  }
+	function handlePreviousQuestion() {
+		if (!data) return;
+		if (currentQuestion - 1 >= 0) {
+			setCurrentQuestion(currentQuestion - 1);
+		}
+	}
 
-  useEffect(() => {
-    if (currentQuestion === -1) return;
-    setQuery((prev) => {
-      prev.set("index", currentQuestion.toString());
-      return prev;
-    });
-  }, [currentQuestion]);
+	useEffect(() => {
+		if (currentQuestion === -1) return;
+		setQuery((prev) => {
+			prev.set("index", currentQuestion.toString());
+			return prev;
+		});
+	}, [currentQuestion]);
 
-  return (
-    <>
-      {currentQuestion !== -1 && data?.[currentQuestion] && (
-        <QuestionLoader
-          question={data[currentQuestion]}
-          answerCallback={() => null}
-        />
-      )}
+	return (
+		<>
+			{currentQuestion !== -1 && data?.[currentQuestion] && (
+				<QuestionLoader
+					question={data[currentQuestion]}
+					answerCallback={() => null}
+				/>
+			)}
 
-      {currentQuestion !== -1 && data?.[currentQuestion] && (
-        <Stack
-          style={{
-            position: "fixed",
-            bottom: 20,
-            left: 10,
-            zIndex: 999,
-          }}
-          id="debugger"
-        >
-          <QuestionInfo
-            questions={data}
-            next={handleNextQuestion}
-            previous={handlePreviousQuestion}
-            current={currentQuestion}
-            total={data.length}
-            onQuestionChange={setCurrentQuestion}
-          />
-        </Stack>
-      )}
-    </>
-  );
+			{currentQuestion !== -1 && data?.[currentQuestion] && (
+				<Stack
+					style={{
+						position: "fixed",
+						bottom: 20,
+						left: 10,
+						zIndex: 999,
+					}}
+					id="debugger"
+				>
+					<QuestionInfo
+						questions={data}
+						next={handleNextQuestion}
+						previous={handlePreviousQuestion}
+						current={currentQuestion}
+						total={data.length}
+						onQuestionChange={setCurrentQuestion}
+					/>
+				</Stack>
+			)}
+		</>
+	);
 }

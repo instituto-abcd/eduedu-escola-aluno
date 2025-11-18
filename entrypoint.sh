aa15df7cd5a20c5ccdb1e00489a1e7757f48bd44
BacchoTerra <brunobacchot@hotmail.com>
2025-11-18 15:35:44 -0300

    Correção de nomes de variaveis em entrypoint.sh, agora acessando corretamente variaveis setada no "environment" do docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 CONFIG_FILE_PATH=/usr/share/nginx/html/config.js
 
 echo "Gerando arquivo de configuração em ${CONFIG_FILE_PATH}..."
-echo "Config: APP_URL: ${APP_URL}"
+echo "Config: API_URL: ${API_URL}"
+echo "Config: ADMIN_URL: ${ADMIN_URL}"
 echo "Config: APP_VERSION: ${APP_VERSION}"
-echo "Config: ADMIN_PORT: ${ADMIN_PORT}"
 
 cat <<EOF > ${CONFIG_FILE_PATH}
 window.config = {
-  API_URL: "${APP_URL}",
-  ADMIN_URL: "${APP_URL}:${ADMIN_PORT}",
+  API_URL: "${API_URL}",
+  ADMIN_URL: "${ADMIN_URL}",
   APP_VERSION: "${APP_VERSION}"
 };
 EOF

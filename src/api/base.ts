db5d3fce8da6d7f38036ba382c59f3b34c58fd1b
Dangocan <danilogc1999@gmail.com>
2026-03-06 16:50:26 -0300

    EEP-535: Utilizar .env em repos front

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,8 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
-import { env } from "~/env";
 
 export class API {
 	static readonly api: AxiosInstance = axios.create({
-		baseURL: window.config.API_URL,
+		baseURL: import.meta.env.VITE_API_URL,
 	});
 }
